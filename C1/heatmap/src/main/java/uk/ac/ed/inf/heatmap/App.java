091c5f812543f6b50fcb6b31960fd4e7eee9f0c5
DawsonSilkenat <47600004+DawsonSilkenat@users.noreply.github.com>
2020-09-27 14:01:56 +0100

    looking at making this work, still very much experimental

--- C1/heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
+++ C1/heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
@@ -2,18 +2,30 @@ package uk.ac.ed.inf.heatmap;
 
 import com.mapbox.geojson.*;
 import java.util.Scanner;
-
+import java.util.List;
 
 public class App {
     
+    /* 
+     * These are values our program depends on stored as variables so they can be easily changed. 
+     * We have assumed our confinement area is defined by a maximum and minimum for both longitude and latitude
+     */
+    private static final double MINIMUM_LONGITUDE = -3.184319;
+    private static final double MAXIMUM_LONGITUDE = -3.192473;
+    private static final double MINIMUM_LATITUDE = 55.942617;
+    private static final double MAXIMUM_LATITUDE = 55.946233;
+    private static final int GRID_WIDTH = 10;
+    private static final int GRID_HEIGHT = 10;
+    private static final double FILL_OPACITY = 0.75;
     
     public static void main(String[] args) { 
         Scanner input = new Scanner(args[0]);
         input.useDelimiter(",");
+        
     }
     
     // Maps an air quality estimate to its intended color code
-    private static String getColor(int airQuality) {
+    private static String getAirQualityColor(int airQuality) {
         if (airQuality >= 0 && airQuality < 32) {
             return "#00ff00";
         } else if (airQuality >= 32 && airQuality < 64) {
