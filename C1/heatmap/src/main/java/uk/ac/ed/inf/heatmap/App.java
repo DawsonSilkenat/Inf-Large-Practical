ec99665527df5d81ac00efa2cff807f69689fa3d
DawsonSilkenat <47600004+DawsonSilkenat@users.noreply.github.com>
2020-09-27 11:33:55 +0100

    small update

--- C1/heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
+++ C1/heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
@@ -1,14 +1,36 @@
 package uk.ac.ed.inf.heatmap;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("Testing");
+import com.mapbox.geojson.*;
+import java.util.Scanner;
+
+
+public class App {
+    
+    
+    public static void main(String[] args) { 
+        Scanner input = new Scanner(args[0]);
+        input.useDelimiter(",");
+    }
+    
+    // Maps an air quality estimate to its intended color code
+    private static String getColor(int airQuality) {
+        if (airQuality >= 0 && airQuality < 32) {
+            return "#00ff00";
+        } else if (airQuality >= 32 && airQuality < 64) {
+            return "#40ff00";
+        } else if (airQuality >= 64 && airQuality < 96) {
+            return "#80ff00";
+        } else if (airQuality >= 96 && airQuality < 128) {
+            return "#c0ff00";
+        } else if (airQuality >= 128 && airQuality < 160) {
+            return "#ffc000";
+        } else if (airQuality >= 160 && airQuality < 192) {
+            return "#ff8000";
+        } else if (airQuality >= 192 && airQuality < 224) {
+            return "#ff4000";
+        } else if (airQuality >= 224 && airQuality < 256) {
+            return "#ff0000";
+        }
+        throw new IllegalArgumentException("Air quality was not within the excepted range");
     }
 }
